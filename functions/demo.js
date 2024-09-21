4211fea31ec32cc32ee9608238622f85ee489756
Adithyas2003 <adathyas49@gmail.com>
2024-09-21 11:28:21 +0530

    java forms

---
+++ functions/demo.js
@@ -0,0 +1,19 @@
+// userdefined function :
+
+function sample(a,b){
+    console.log(a,b);
+
+}
+sample(10,20)
+
+// arrowfunction:
+Data=(a,b)=>{
+    console.log("js arrow",a,b);
+}
+Data(10,20)
+
+// namelessfunction:
+data=function(a,b){
+    console.log("nameless function",a,b);
+}
+data(20,30)
---
+++ stringmethods/demo.js
@@ -0,0 +1,24 @@
+
+// a="welcome"
+// b="Hello Welcome"
+// console.log(a.length);
+// console.log(b.toUpperCase())
+// console.log(b.toLowerCase())
+// console.log(b.trim(),"ddj");
+// console.log(b.trimstart(),"ddj");
+// console.log(b.trimEnd(),"ddj")
+// console.log(a.charAt(0));
+// console.log(a.charcodeAt(0));
+
+// match:
+a="Hello welcome to all"
+console.log(a.slice(2,6));
+console.log(a.match("Hello"))
+console.log(a.match("[a-z].+"))
+console.log(a.match("[a-z0-9]"))
+console.log(a.match("[a-z].*"))
+console.log(a.match("[a-z].?"))
+
+
+// search:
+console.log(a.search("Hellow"));
