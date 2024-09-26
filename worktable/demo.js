051811177879b89d810357f410978cd5a85c4474
Adithyas2003 <adathyas49@gmail.com>
2024-09-26 11:31:00 +0530

    wrktable

--- worktable/demo.js
+++ worktable/demo.js
@@ -18,6 +18,15 @@ function display(){
         t_age.textContent=element.age
         t_row.appendChild(t_age)
 
+        const edit_id=document.createElement('td')
+        const edit_btn=document.createElement('button')
+        edit_btn.textContent='edit'
+        edit_btn.onclick=function(){
+            edit_frm(element.id)
+        }
+        edit_id.appendChild(edit_btn)
+        t_row.appendChild(edit_id)
+
 
         t_var.appendChild(t_row)
     });
@@ -33,17 +42,34 @@ document.getElementById('name').value=''
 document.getElementById('age').value=''
 display()
 })
+let editid=''
+function edit_frm(id){
+    console.log('editing',id);
+    document.getElementById('editform').style.display='block'
+    document.getElementById('submitform').style.display='none'
+    const edit_data=d.find(user=>user.id==id)
+    document.getElementById('e_id').value=edit_data.id
+    document.getElementById('e_name').value=edit_data.name
+    document.getElementById('e_age').value=edit_data.age
+    editid=id
+}
 
-document.getElementById('editform').addEventListener('edit',function(event) {
+document.getElementById('editform').addEventListener('submit',function(event){
     event.preventDefault()
-const id=document.getElementById('id').value
-const name=document.getElementById('name').value
-const age= parseInt(document.getElementById('age').value)
-d.push({id:id,name:name,age:age})
-document.getElementById('id').value=''
-document.getElementById('name').value=''
-document.getElementById('age').value=''
-display()
+    const e_id=document.getElementById('e_id').value
+    const e_name=document.getElementById('e_name').value
+    const e_age=document.getElementById('e_age').value
+    d=d.map(user=>{
+        if(user.id==editid){
+            return {...user,id:e_id,name:e_name,age:e_age}
+
+        }
+        return user
+    })
+    document.getElementById('editform').style.display='none'
+    document.getElementById('submitform').style.display='block'
+    display()
+
 })
 
 
