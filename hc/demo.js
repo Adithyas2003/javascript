86d5d2494b359452402f6c4e79ca9312ee957850
Adithyas2003 <adathyas49@gmail.com>
2024-10-03 11:29:31 +0530

    API programs

--- hc/demo.js
+++ hc/demo.js
@@ -36,37 +36,35 @@
 // }
 // demo()
 
+
+// function demo(){
+//     let b='hai hello'
+//     console.log(b);
+//     const main=document.querySelector('.main')
+//     const div=document.createElement('div')
+//     // div.innerHTML=`<h2 style="color:blue">novavi ${b}</h2>`
+//     div.innerHTML=`<p style="color:green">novavi asafvgvgbbbbbblm ${b}</p>`
+
+//     main.appendChild(div)
+
+// }
+// demo()
+
 function demo(){
     let b='hai hello'
     console.log(b);
     const main=document.querySelector('.main')
     const div=document.createElement('div')
     let count=59
-    let count1=59
-    
     let c=setInterval(function(){
     div.innerHTML=`<p style="color:green">03:${count}</p>`
-    div.innerHTML=`<p style="color:green">02:${count}</p>`
-    
-
     count--
     if(count==0){
         clearInterval(c)
         console.log('completed');
         div.innerHTML=`<h2 style="color:green"> completed</h2>`
-        
-        
     }
     },1000)
-    if(count1==2){
-        clearInterval(c)
-        console.log('completed');
-        div.innerHTML=`<h2 style="color:green"> completed</h2>`
-        
-        
-    }
-    },1000)
-
         
    
    
---
+++ oops/demo.js
@@ -0,0 +1,38 @@
+// class animals {
+//     constructor(name,age) {
+//         this.name=name
+//         this.age=age
+        
+//     }
+//     display(){
+//         console.log(this.name,this.age);
+
+        
+//     }
+// }
+// cat=new animals('cat',2)
+// cat.display()
+
+
+// d={
+//     f_name:"Arun",
+//     l_name:"manoj",
+//     age:20,
+//     full_name:function(){
+//         return this.f_name+this.l_name
+//     }
+
+// }
+// console.log(d.full_name());
+
+
+// exception handling:
+
+
+try {
+    alertt('welcome')
+} catch (error) {
+    console.log('error');
+    
+    
+}
