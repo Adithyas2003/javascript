ff5fb4d3b9203600d4f43a7b843b8af10f412e32
Adithyas2003 <adathyas49@gmail.com>
2024-10-04 11:26:40 +0530

    API Programs added

--- API/demo.js
+++ API/demo.js
@@ -5,11 +5,38 @@ fetch('https://dummyjson.com/products').then(response=>response.json()).then(dat
         const col =document.createElement("div")
         col.classList.add('col-lg-4')
         col.innerHTML=`
-        <img src="${element.images}" width="100%"
-        <h3>${element.title}</h3>
-        <h6>${element.price}</h6>
+        <div class="card">
+            <img class="card-img-top" src="${element.images}" alt="card image" width="100%">
+            <div class="card-body">
+            <h3 class="card-title">${element.title}</h3>
+            
+            <p class="card-text">${element.description}</p>
+                <a href="#" class="btn btn-primary see-more-btn" data-id="${element.id}" data-bs-toggle="modal" data-bs-target="#productModal">see more</a>
+            </div>
+        </div>
+
         `
         row.appendChild(col)
         
     });
+    document.querySelectorAll('.see-more-btn').forEach(btn =>{
+        btn.addEventListener('click', (e)=>{
+            const productId =e.target.getAttribute('data-id');
+            const product =data.products.find(p =>p.id == productId);
+
+            if (product) {
+                document.getElementById('modalImage').src =product.thumbnail;
+                document.getElementById('modalTitle').textContent =product.title;
+                document.getElementById('modalDescription').textContent =product.description;
+                document.getElementById('modalPrice').textContent =`price: $${product.price}`;
+
+                
+            }
+        });
+    });
+
+
 })
+.catch(error =>{
+    console.error("error fetching the date:",error);
+});
